3c09112c8e4d0a4e70b897ad56e047d71301296a
Xi Chen <xchen339@myseneca.ca>
2023-05-17 19:05:46 -0400

    Set up passport authentication middleware

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const compression = require('compression');
+const passport = require('passport');
+const authenticate = require('./authentication');
 
 // version and author from our package.json file
 const { version, author } = require('../package.json');
@@ -28,6 +30,10 @@ app.use(cors());
 
 // Use gzip/deflate compression middleware
 app.use(compression());
+
+// Set up our passport authentication middleware
+passport.use(authenticate.strategy());
+app.use(passport.initialize());
 
 // Define our routes
 app.use('/', require('./routes'));
