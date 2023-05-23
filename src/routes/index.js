fa34a81b477f1797dd157f4c870e9e1981575b9d
Xi Chen <xchen339@myseneca.ca>
2023-05-23 11:35:49 -0400

    Call function to generate GET response

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,8 @@ const express = require('express');
 // version and author from package.json
 const { version, author } = require('../../package.json');
 
+const { createSuccessResponse } = require('../response');
+
 // Create a router that we can use to mount our API
 const router = express.Router();
 
@@ -22,15 +24,14 @@ router.use(`/v1`, authenticate(), require('./api'));
  * we'll respond with a 200 OK.  If not, the server isn't healthy.
  */
 router.get('/', (req, res) => {
-  // Client's shouldn't cache this response (always request it fresh)
+  // Clients shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
-  // Send a 200 'OK' response
-  res.status(200).json({
-    status: 'ok',
-    author,
-    githubUrl: 'https://github.com/suscx/fragments',
-    version,
-  });
+
+  // Send a 200 'OK' response and some additional info
+  const data = { author, githubUrl: 'https://github.com/suscx/fragments', version };
+  
+  res.status(200).json(createSuccessResponse(data));
+
 });
 
 module.exports = router;
