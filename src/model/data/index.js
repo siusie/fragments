a2c4192f94a64094ed0b5b0532efb026917e1f67
Xi Chen <xchen339@myseneca.ca>
2023-05-28 19:23:09 -0400

    Init

---
+++ src/model/data/index.js
@@ -0,0 +1,5 @@
+// src/model/data/index.js
+// This file chooses the appropriate back-end data strategy.
+// Currently (as of Assignment 1), there is one strategy, `memory`.
+
+module.exports = require('./memory');
