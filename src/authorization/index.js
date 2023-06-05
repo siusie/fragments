2a2cf00780d0debc9f262d93f23eed585c332c24
Xi Chen <xchen339@myseneca.ca>
2023-06-05 12:43:15 -0400

    Add logging messages

--- src/authorization/index.js
+++ src/authorization/index.js
@@ -1,12 +1,16 @@
 // src/authorization/index.js
 
+const logger = require('../logger');
+
 // Prefer Amazon Cognito
 if (process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID) {
   module.exports = require('./cognito');
+  logger.info(`[Authorization strategy] Cognito`);
 }
 // Also allow for an .htpasswd file to be used, but not in production
 else if (process.env.HTPASSWD_FILE && process.NODE_ENV !== 'production') {
   module.exports = require('./basic-auth');
+  logger.info(`[Authorization strategy] Basic Auth`);
 }
 // In all other cases, we need to stop now and fix our config
 else {
