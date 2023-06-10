8da510767a733dd825fcc45bf4265e0747c64280
Xi Chen <xchen339@myseneca.ca>
2023-06-10 16:07:01 -0400

    remove comments

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -6,10 +6,7 @@ const { Fragment } = require('../../model/fragment');
 const contentType = require('content-type');
 
 module.exports = (req, res) => {
-
-  // logger.debug(`[post.js] Incoming req.body is a buffer? ${Buffer.isBuffer(req.body)} and its length is ${Buffer.byteLength(req.body)}`);
-  // logger.debug(`current user: ${(req.user)}`);
-
+  
   // generate an error response if:
   // content type is invalid (req.body is not a buffer),
   // file is empty (buffer bytes = 0)
@@ -30,13 +27,9 @@ module.exports = (req, res) => {
         logger.debug(`SAVED DATA SUCCESSFULLY`);
 
         res
-          .setHeader('Location', `${process.env.API_URL || req.headers.host}/v1/fragments/${fragment.id}`)
-          .setHeader('Content-Type', type)
+          .setHeader('Location', `${req.headers.host}/v1/fragments/${fragment.id}`)
           .status(201)
-          .json(createSuccessResponse({fragment}));
-        // logger.debug(`got back: ${JSON.stringify(createSuccessResponse({fragment}))}`); 
-        // const data = fragment.getData().then((d) => { d.string }).catch(logger.info(`ERROR`));
-        // logger.debug(`created fragment - ownerId: ${fragment.ownerId}, type: ${fragment.type}, data: ${(data.then((d)=>{d.string}))}`);        
+          .json(createSuccessResponse({fragment}));    
       }))      
       .catch((err) => {
         logger.error(`Error saving fragment: ${err}`);
