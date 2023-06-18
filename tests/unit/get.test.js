9556fc5f3edabdb9cea97576fa3e23e039c62cdf
sucee <xchen339@myseneca.ca>
2023-06-18 13:27:09 -0400

    add unit tests

--- tests/unit/get.test.js
+++ tests/unit/get.test.js
@@ -4,15 +4,18 @@ const request = require('supertest');
 const app = require('../../src/app');
 const logger = require('../../src/logger');
 const { Fragment } = require('../../src/model/fragment');
-const { createErrorResponse } = require('../../src/response');
+const { createErrorResponse, createSuccessResponse } = require('../../src/response');
 const hash = require('../../src/hash');
+
+const fragment1 = new Fragment({ ownerId: hash('user1@email.com'), type: 'text/plain', size: 0 });
+const fragment2 = new Fragment({ ownerId: hash('user1@email.com'), type: 'text/plain', size: 0 });
 
 describe('GET /v1/fragments', () => {
 
   // If the request is missing the Authorization header, it should be forbidden
   test('unauthenticated requests are denied', () => request(app).get('/v1/fragments').expect(401));
 
-  // If the wrong username/password pair are used (no such user), it should be forbidden
+  // If the wrong username/password pair is used (no such user), it should be forbidden
   test('incorrect credentials are denied', () =>
     request(app).get('/v1/fragments').auth('invalid@email.com', 'incorrect_password').expect(401));
 
@@ -22,49 +25,37 @@ describe('GET /v1/fragments', () => {
     expect(res.statusCode).toBe(200);
     expect(res.body.status).toBe('ok');
     expect(Array.isArray(res.body.fragments)).toBe(true);
-  });  
+  });
 
-  test('retrieve a list of the current user\'s fragments', async () => {
-    const fragment1 = new Fragment({ ownerId: hash('user2@email.com'), type: 'text/plain', size: 0 });
+  test('retrieve a list of fragment IDs', async () => {
     await fragment1.save();
-    await fragment1.setData(Buffer.from('Fragment 1'));
-
-    const fragment2 = new Fragment({ ownerId: hash('user2@email.com'), type: 'text/plain' });
     await fragment2.save();
-    await fragment2.setData(Buffer.from('Fragment 2'));
-
-    const fragment3 = new Fragment({ ownerId: hash('user2@email.com'), type: 'text/plain; charset=utf-8', size: 0 });
+    const fragment3 = new Fragment({ ownerId: hash('user1@email.com'), type: 'text/plain; charset=utf-8', size: 0 });
     await fragment3.save();
-    await fragment3.setData(Buffer.from('Fragment 3'));
-
-    const res = await request(app).get('/v1/fragments').auth('user2@email.com', 'password2');
-
-    // logger.debug(`got back: ${(res.body.fragments)}`)
+    const res = await request(app).get('/v1/fragments').auth('user1@email.com', 'password1');
+    logger.debug(`got back: ${(res.body.fragments)}`)
     expect(res.statusCode).toBe(200);
     expect(res.body.status).toBe('ok');
     expect(Array.isArray(res.body.fragments)).toBe(true);
     expect(res.body.fragments.length).not.toBe(0);
+    expect(typeof res.body.fragments[0]).not.toBe('object');
   });
   
 });
 
 describe('GET /v1/fragments?expand=1', () => {
   test('list of fragments contains their metadata', async () => { 
-    const fragment1 = new Fragment({ ownerId: hash('user1@email.com'), type: 'text/plain', size: 0 });
     await fragment1.save();
-    await fragment1.setData(Buffer.from('Fragment 1'));
-
-    const fragment2 = new Fragment({ ownerId: hash('user1@email.com'), type: 'text/plain' });
+    await fragment1.setData(Buffer.from('fjslkfjalsdjflkajsdl'));
     await fragment2.save();
-    await fragment2.setData(Buffer.from('Fragment 2'));
-
+    await fragment2.setData(Buffer.from('fjslkfjalsdjflkajsdlfdsfdsdsafdsafsdfdsafdsaads'));
     const fragment3 = new Fragment({ ownerId: hash('user1@email.com'), type: 'text/plain; charset=utf-8', size: 0 });
-    await fragment3.save();
-    await fragment3.setData(Buffer.from('Fragment 3'));
+    // fragment3.save();  // TODO: fix save() and setData() -- 2 instances of fragment3 created
+    await fragment3.setData(Buffer.from('hello'));
 
     const res = await request(app).get('/v1/fragments?expand=1').auth('user1@email.com', 'password1');
 
-    // logger.debug(`got back: ${(res.body.fragments)}`);
+    logger.debug(`got back: ${JSON.stringify(res.body.fragments, null, 4)}`);
     expect(res.statusCode).toBe(200);
     expect(res.body.status).toBe('ok');
     expect(Array.isArray(res.body.fragments)).toBe(true);
@@ -136,17 +127,55 @@ describe('GET /v1/fragments/:id', () => {
   });
 
   test('unsupported extension results in 415 error', async () => {
-    const fragment = new Fragment({ ownerId: 'user2@email.com', type: 'text/plain', size: 0 });
-    await fragment.save();
-    await fragment.setData(Buffer.from('TEST FRAGMENT'));
+    await fragment1.save();
+    await fragment1.setData(Buffer.from('TEST FRAGMENT'));
 
     const res = await request(app)
-      .get(`/v1/fragments/${fragment.id}.html`)
-      .auth('user2@email.com', 'password2');
+      .get(`/v1/fragments/${fragment1.id}.html`)
+      .auth('user1@email.com', 'password1');
     
     const errorResponse = createErrorResponse(415, `unable to convert to html`);    
     logger.debug(`got back: ${JSON.stringify(res.body, null, 4)}`);    
     expect(res.statusCode).toBe(415);
     expect(res.body).toStrictEqual(errorResponse);
+  });
+});
+
+describe('GET /fragments/:id/info', () => {
+  test('retrieve a fragment\'s metadata', async () => { 
+    await fragment1.save();
+    await fragment1.setData(Buffer.from('Allows the authenticated user to get (i.e., read) the metadata for one of their existing fragments with the specified id. If no such fragment exists, returns an HTTP 404 with an appropriate error message.'));
+
+    const res = await request(app)
+      .get(`/v1/fragments/${fragment1.id}/info`)
+      .auth('user1@email.com', 'password1')
+    
+    const successResponse = createSuccessResponse({fragment: fragment1});        
+    expect(res.statusCode).toBe(200);    
+    expect(res.body).toEqual(successResponse);
+  });
+
+  test('including an extension in ID generates error', async () => {    
+    await fragment1.save();
+    await fragment1.setData(Buffer.from('Allows the authenticated user to get (i.e., read) the metadata for one of their existing fragments with the specified id. If no such fragment exists, returns an HTTP 404 with an appropriate error message.'));
+
+    const res = await request(app)
+      .get(`/v1/fragments/${fragment1.id}.txt/info`)
+      .auth('user1@email.com', 'password1');
+    
+    const errorResponse = createErrorResponse(404, `Invalid ID, got ${fragment1.id}.txt`);        
+    expect(res.statusCode).toBe(404);
+    expect(res.body).toEqual(errorResponse);
+  });
+
+  test('invalid ID generates error', async () => {
+    await fragment1.save();
+    await fragment1.setData(Buffer.from('Test fragment'));
+
+    const res = await request(app)
+      .get(`/v1/fragments/${fragment1.id}./info`)
+      .auth('user1@email.com', 'password1');
+         
+    expect(res.statusCode).toBe(404);
   });
 });
