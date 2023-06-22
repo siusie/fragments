89602ea4f6bac019b9dbec8742a70dab6a033a79
sucee <xchen339@myseneca.ca>
2023-06-22 11:37:42 -0400

    use async/await instead of .then

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -5,32 +5,34 @@ const logger = require('../../logger');
 const { Fragment } = require('../../model/fragment');
 const contentType = require('content-type');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   
   // generate an error response if:
   // content type is invalid (req.body is not a buffer),
   // file is empty (buffer bytes = 0)
   if (!Buffer.isBuffer(req.body) || Buffer.byteLength(req.body) === 0) { 
-    res.status(415).json(createErrorResponse(415, `content-type is not supported`));
-  } else {
-    const { type } = contentType.parse(req);
-    logger.debug(`[post.js] type: ${type}`);
-    
-    // create a new fragment object and save it to the db
-    let fragment = new Fragment({ ownerId: req.user, type: type });
+    return res.status(415).json(createErrorResponse(415, `content-type is not supported`));
+  } 
 
-    fragment
-      .save()
-      .then(fragment.setData(req.body).then(() => {
-        logger.debug(`SAVED DATA SUCCESSFULLY`);
+  // getting the content type (specified by the client)
+  const { type } = contentType.parse(req);
+  logger.debug(`[post.js] type: ${type}`);
+  
+  // create a new fragment object
+  let fragment = new Fragment({ ownerId: req.user, type: type });
 
-        res
-          .setHeader('Location', `${req.headers.host}/v1/fragments/${fragment.id}`)
-          .status(201)
-          .json(createSuccessResponse({fragment}));    
-      }))      
-      .catch((err) => {
-        logger.error(`Error saving fragment: ${err}`);
-      });
+  // Attempt to save fragment to the db
+  try {
+    await fragment.save();
+    await fragment.setData(req.body);
+    logger.debug(`SAVED DATA SUCCESSFULLY`);
+
+    res
+      .setHeader('Location', `${req.protocol}://${process.env.API_URL}/v1/fragments/${fragment.id}`)
+      .status(201)
+      .json(createSuccessResponse({fragment}));  
+  }
+  catch (err) {
+      logger.error(`Error saving fragment - ${err}`);    
   }
 };
