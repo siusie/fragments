4e5625fcf19d4b76be7a51042b74e7962129f1ae
sucee <xchen339@myseneca.ca>
2023-07-11 13:30:35 -0400

    Set Content-Type header to the correct type in response object

--- src/routes/api/get-data.js
+++ src/routes/api/get-data.js
@@ -14,7 +14,6 @@ module.exports = async (req, res) => {
  
   const extension = path.extname(req.params.id).toLowerCase();
   const type = mime.lookup(extension); // false if extension is invalid or nonexistent
-  logger.debug(`[get.js] The content-type associated with ${extension} is ${type}`);
   
   // Return a 415 error response if the extension is unknown
   if (extension !== '' && !type) {
@@ -23,23 +22,20 @@ module.exports = async (req, res) => {
 
   // Extract fragment id w/o extension.
   // Note: path.basename() treats the extension as a case-sensitive string 
-  // and won't extract id if extension's case and req.originalUrl's case do not match, 
-  // so they're both in lowercase
+  // and won't extract id if extension's case and req.originalUrl's case do not match so convert both to lowercase
   const id = extension ? path.basename(req.originalUrl.toLowerCase(), extension) : req.params.id;
   logger.debug(`got fragment id: ${id}, original URL: ${req.originalUrl}`);
 
   // At this point, id contains either no extension or a valid extension (which may or may not be supported)
   try {
-    const fragment = await Fragment.byId(req.user, id); // retrieve the fragment
-    let data;
-
+    const fragment = await Fragment.byId(req.user, id); // attempt to retrieve the fragment
+    
     // If type === false here, it would mean that there's no extension --> retrieve the binary data
     // else there is an extension --> attempt to convert to that type
-    data = type ? await fragment.convertData(type) : await fragment.getData();
-    logger.debug(`got fragment data: ${data}`);
+    const data = type ? await fragment.convertData(type) : await fragment.getData();
     
     // `data` will be an empty string if attempting to convert to an unsupported content-type
-    return data ? res.status(200).setHeader('Content-Type', type).send(data) : res.status(415).json(createErrorResponse(415, `Unable to convert to ${extension}`));
+    return data ? res.status(200).setHeader('Content-Type', type ? type : fragment.type).send(data) : res.status(415).json(createErrorResponse(415, `Unable to convert to ${extension}`));
   } catch (err) {
     logger.error(err);
     return res.status(404).json(createErrorResponse(404, err.message));
