233763051f1b937aea6091f9d373a32c1610baa2
sucee <xchen339@myseneca.ca>
2023-07-28 14:14:06 -0400

    Remove unnecessary code

--- src/routes/api/get-metadata.js
+++ src/routes/api/get-metadata.js
@@ -10,7 +10,6 @@ const { Fragment } = require('../../model/fragment');
 module.exports = async (req, res) => { 
   try {
     const fragment = await Fragment.byId(req.user, req.params.id);
-    logger.info(`got fragment: ${JSON.stringify(fragment, null, 4)}`);
     return res.status(200).json(createSuccessResponse({ fragment }));
   } catch (err) {
     logger.error(err);
