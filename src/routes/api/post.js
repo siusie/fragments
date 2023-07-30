47c734725e6ea507b2128fcb22dad7de8f5b199e
sucee <xchen339@myseneca.ca>
2023-07-30 14:03:53 -0400

    Re-format the try-catch block

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -5,29 +5,28 @@ const logger = require('../../logger');
 const { Fragment } = require('../../model/fragment');
 const contentType = require('content-type');
 
-module.exports = async (req, res) => {
-  // Generate an error response if:
-  // content type is invalid (req.body is not a buffer),
-  // file is empty (buffer bytes = 0)
-  if (!Buffer.isBuffer(req.body) || !Buffer.byteLength(req.body)) { 
-    return res.status(415).json(createErrorResponse(415, 'content-type is not supported'));
-  }
-
-  // Empty fragments are not allowed 
-  if (!req.body.toString().replace(/\s/g, '').length) {
-    return res.status(403).json(createErrorResponse(403, 'fragment data cannot be empty'));
-  }
-
-  // getting the content type (specified by the client)
-  const type = contentType.parse(req);
-  // logger.debug(`[post.js] parsed type: ${ JSON.stringify(type, null, 4) }}`);
-
-  
-  // Create a new fragment object, making sure to include the character set as well (if it was specified during creation)
-  let fragment = new Fragment({ ownerId: req.user, size: Buffer.byteLength(req.body), type: type.parameters.charset ? type.type + '; charset=' + type.parameters.charset : type.type });
-
-  // Attempt to save fragment to the db
+module.exports = async (req, res) => {  
   try {
+    // Generate an error response if
+    // content-type is invalid (req.body is not a buffer)
+    if (!Buffer.isBuffer(req.body)) { 
+      return res.status(415).json(createErrorResponse(415, 'fragment type is not supported'));
+    }
+
+    // Empty fragments are not allowed
+    if (!req.body.toString().replace(/\s/g, '').length || !Buffer.byteLength(req.body)) {
+      return res.status(400).json(createErrorResponse(400, 'fragment data cannot be empty'));
+    }
+
+    // getting the content type (specified by the client)
+    const type = contentType.parse(req);
+    // logger.debug(`[post.js] parsed type: ${ JSON.stringify(type, null, 4) }}`);
+
+    
+    // Create a new fragment object, making sure to include the character set as well (if it was specified during creation)
+    let fragment = new Fragment({ ownerId: req.user, size: Buffer.byteLength(req.body), type: type.parameters.charset ? type.type + '; charset=' + type.parameters.charset : type.type });
+
+    // Attempt to save fragment to the db  
     await fragment.save();
     await fragment.setData(req.body);
 
