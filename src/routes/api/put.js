53d594191f60ea9d1291519fa5b6e7c6fe24d809
sucee <xchen339@myseneca.ca>
2023-08-16 11:09:15 -0400

    Change error message

--- src/routes/api/put.js
+++ src/routes/api/put.js
@@ -14,12 +14,13 @@ module.exports = async (req, res) => {
     // Generate an error response if
     // Content-Type is invalid (req.body is not a buffer)
     if (!Buffer.isBuffer(req.body)) {
-      return res.status(415).json(createErrorResponse(415, 'invalid_content_type'));
+      return res.status(415).json(createErrorResponse(415, 'UNSUPPORTED_CONTENT_TYPE'));
     }
 
     // Empty fragments are not allowed
     if (!req.body.toString().replace(/\s/g, '').length) {
-     return !Buffer.isBuffer(req.body) ?  res.status(400).json(createErrorResponse(400, 'A fragment\'s type cannot be changed!')) : res.status(400).json(createErrorResponse(400, 'fragment data cannot be empty'));
+    //  return !Buffer.isBuffer(req.body) ?  res.status(400).json(createErrorResponse(400, 'A fragment\'s type cannot be changed!')) : res.status(400).json(createErrorResponse(400, 'EMPTY_DATA'));
+     return res.status(400).json(createErrorResponse(400, 'NO_EMPTY_CONTENT'));
     }
 
     // Attempt to retrieve the fragment
@@ -32,7 +33,7 @@ module.exports = async (req, res) => {
     const type = parsedType.parameters.charset ? parsedType.type + '; charset=' + parsedType.parameters.charset : parsedType.type;
 
     if (type !== fragment.type) {
-      return res.status(400).json(createErrorResponse(400, 'A fragment\'s type cannot be changed!'));
+      return res.status(400).json(createErrorResponse(400, 'CANT_CHANGE_FRAGMENT_TYPE'));
     }
 
     // At this point, the fragment exists and the data + Content-Type in the request are valid 
